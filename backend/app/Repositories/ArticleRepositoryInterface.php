35d6b776046b8ff82ecb35ce307d51beeb1302f7
take-cantik <take.cantik17@gmail.com>
2022-02-22 21:34:37 +0900

    Add: Dto type to repository

--- backend/app/Repositories/ArticleRepositoryInterface.php
+++ backend/app/Repositories/ArticleRepositoryInterface.php
@@ -2,11 +2,16 @@
 
 namespace App\Repositories;
 
+use App\Http\Dtos\Article\GetListDto;
+use App\Http\Dtos\Article\FindByIdDto;
+use App\Http\Dtos\Article\CreateDto;
+use App\Http\Dtos\Article\UpdateDto;
+
 interface ArticleRepositoryInterface
 {
-    public function getArticleList($getListDto);
-    public function getArticle($findByIdDto);
-    public function createArticle($createDto);
-    public function updateArticle($updateDto);
-    public function deleteArticle($findByIdDto);
+    public function getArticleList(GetListDto $getListDto);
+    public function getArticle(FindByIdDto $findByIdDto);
+    public function createArticle(CreateDto $createDto);
+    public function updateArticle(UpdateDto $updateDto);
+    public function deleteArticle(FindByIdDto $findByIdDto);
 }
--- backend/app/Repositories/Eloquent/ArticleRepository.php
+++ backend/app/Repositories/Eloquent/ArticleRepository.php
@@ -4,8 +4,35 @@ namespace App\Repositories\Eloquent;
 
 use App\Repositories\ArticleRepositoryInterface;
 use App\Models\Article AS ArticleModel;
+use App\Http\Dtos\Article\GetListDto;
+use App\Http\Dtos\Article\FindByIdDto;
+use App\Http\Dtos\Article\CreateDto;
+use App\Http\Dtos\Article\UpdateDto;
 
 class ArticleRepository implements ArticleRepositoryInterface
 {
-    //
+    public function getArticleList(GetListDto $getListDto)
+    {
+        //
+    }
+
+    public function getArticle(FindByIdDto $findByIdDto)
+    {
+        //
+    }
+
+    public function createArticle(CreateDto $createDto)
+    {
+        //
+    }
+
+    public function updateArticle(UpdateDto $updateDto)
+    {
+        //
+    }
+
+    public function deleteArticle(FindByIdDto $findByIdDto)
+    {
+        //
+    }
 }
