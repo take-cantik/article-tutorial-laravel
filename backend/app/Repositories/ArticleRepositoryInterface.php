b91c19a0a5f871c8b381ceedbe28345acae5ab26
take-cantik <take.cantik17@gmail.com>
2022-02-24 12:46:31 +0900

    Add: entity to repository

--- backend/app/Repositories/ArticleRepositoryInterface.php
+++ backend/app/Repositories/ArticleRepositoryInterface.php
@@ -6,11 +6,13 @@ use App\Http\Dtos\Article\GetListDto;
 use App\Http\Dtos\Article\FindByIdDto;
 use App\Http\Dtos\Article\CreateDto;
 use App\Http\Dtos\Article\UpdateDto;
+use App\Entities\Article\ArticleList;
+use App\Entities\Article\Article;
 
 interface ArticleRepositoryInterface
 {
-    public function getArticleList(GetListDto $getListDto);
-    public function getArticle(FindByIdDto $findByIdDto);
+    public function getArticleList(GetListDto $getListDto): ArticleList;
+    public function getArticle(FindByIdDto $findByIdDto): Article;
     public function createArticle(CreateDto $createDto);
     public function updateArticle(UpdateDto $updateDto);
     public function deleteArticle(FindByIdDto $findByIdDto);
