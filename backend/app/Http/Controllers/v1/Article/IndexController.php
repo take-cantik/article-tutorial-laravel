a57cfacc011fb5dceb30c35109cb2dafdbef45f5
take-cantik <take.cantik17@gmail.com>
2022-02-24 14:51:20 +0900

    Update: show articles

--- backend/app/Http/Controllers/v1/Article/IndexController.php
+++ backend/app/Http/Controllers/v1/Article/IndexController.php
@@ -5,16 +5,21 @@ namespace App\Http\Controllers\v1\Article;
 use App\Http\Controllers\Controller;
 use Illuminate\View\View;
 use App\Http\Requests\Article\GetListRequest;
+use App\Usecases\Article\IndexUsecase;
 use App\Http\Dtos\Article\GetListDto;
 
 class IndexController extends Controller
 {
-    public function __invoke(GetListRequest $request): View
+    public function __invoke(GetListRequest $request, IndexUsecase $indexUsecase): View
     {
-        $getListDto = new GetListDto({
+        $getListDto = new GetListDto([
             'page' => $request->page,
-            'limit' => 12
-        });
-        // return view();
+        ]);
+
+        $articles = $indexUsecase->execute($getListDto);
+
+        dd($articles);
+
+        return view();
     }
 }
