57f61c3b03b0104f3624d926a1d7f74cb28f3f2b
take-cantik <take.cantik17@gmail.com>
2022-02-24 13:19:34 +0900

    Update: use dto and request

--- backend/app/Http/Controllers/v1/Article/IndexController.php
+++ backend/app/Http/Controllers/v1/Article/IndexController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers\v1\Article;
 
 use App\Http\Controllers\Controller;
 use Illuminate\View\View;
-use Illuminate\Http\Request;
+use App\Http\Requests\Article\GetListRequest;
+use App\Http\Dtos\Article\GetListDto;
 
 class IndexController extends Controller
 {
-    public function __invoke(Request $request): View
+    public function __invoke(GetListRequest $request): View
     {
+        $getListDto = new GetListDto({
+            'page' => $request->page,
+            'limit' => 12
+        });
         // return view();
     }
 }
