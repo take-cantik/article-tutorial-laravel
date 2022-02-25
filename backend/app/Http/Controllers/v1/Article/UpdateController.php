e9d7d0d17c7c53b85b3e1f0bdbc1a74bc62a5996
take-cantik <take.cantik17@gmail.com>
2022-02-25 20:12:08 +0900

    Add: update controller process

--- backend/app/Http/Controllers/v1/Article/UpdateController.php
+++ backend/app/Http/Controllers/v1/Article/UpdateController.php
@@ -5,11 +5,22 @@ namespace App\Http\Controllers\v1\Article;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
+use App\Http\Requests\Article\UpdateRequest;
+use App\Usecases\Article\UpdateUsecase;
+use App\Http\Dtos\Article\UpdateDto;
 
 class UpdateController extends Controller
 {
-    public function __invoke(Request $request, integer $id): RedirectResponse
+    public function __invoke(UpdateRequest $request, int $id, UpdateUsecase $usecase): RedirectResponse
     {
-        // return redirect(route());
+        $updateDto = new UpdateDto([
+            'id' => $id,
+            'title' => $request->title,
+            'body' => $request->body,
+            'category' => $request->category,
+            'authorId' => $request->authorId
+        ]);
+        $updateRef = $usecase->execute($updateDto);
+        return redirect(route('articles.show', ['articleId' => $id]));
     }
 }
